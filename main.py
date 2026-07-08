0c66d2cbec567725ef0953c312364ac1a5371971
JMSU-cjie <165013323+JMSU-cjie@users.noreply.github.com>
2026-07-08 18:30:02 +0800

    a

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
 from fastapi import FastAPI
-from starlette.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from starlette.responses import RedirectResponse
 
 app = FastAPI()
+
+# 挂载静态文件目录：所有 /static/xxx 的请求会去 ./static 目录找 xxx
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
 @app.get("/")
 def root():
-    return HTMLResponse(content="""
-    <script>
-        window.location.href = '/user/login.html';
-    </script>
-    """)
-
+    # 访问根路径时，直接重定向到登录页
+    return RedirectResponse(url="/static/login.html")
 
 if __name__ == "__main__":
     import uvicorn
